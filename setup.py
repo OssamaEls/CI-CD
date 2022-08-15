0a0aaf4008053e6dcf840fcb38d7d5f7cdf7079d
Ossama <elsossama@gmail.com>
2022-08-15 12:44:55 +0100

    Fix black argument

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_namespace_packages
 
 if __name__ == "__main__":
     setup(
-        name='ossama_simple_package',
-        version='0.1.0',
-        description='A simple package',
-        packages=find_namespace_packages(where='src'),
-        install_requires='requests>1',
-        extras_require={'tests': ['pytest']}
+        name="ossama_simple_package",
+        version="0.1.0",
+        description="A simple package",
+        packages=find_namespace_packages(where="src"),
+        install_requires="requests>1",
+        extras_require={"tests": ["pytest"]},
     )
